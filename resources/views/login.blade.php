9363dbfad0e7c834c0e5077a1d6a5e210f0b7e12
Roger Noel <noel.roger.pro@gmail.com>
2018-12-14 10:31:59 +0100

    création register, login et ajout des routes dans web.php

---
+++ resources/views/login.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <h1>BEFLIX</h1>
+    <h2>Login</h2>
+    <div>
+        <p>
+            <label for="email">E-mail</label>
+            <input type="email">
+        </p>
+        <p>
+            <label for="password">Password</label>
+            <input type="password"></p>
+    </div>
+</body>
+</html>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register page</title>
+</head>
+<body>
+    <h1>BEFLIX</h1>
+    <h2>Register</h2>
+    <div>
+        <form action="" method="POST">
+            <p>
+                <label for="firstname">Your firstname</label>
+                <input type="text" name="firstname">
+                <label for="lastname">Your lastname</label>
+                <input type="text" name="lastname">
+            </p>
+            <p>
+                <label for="email">Your e-mail</label>
+                <input type="email" name="email">
+                <label for="confirmEmail">Confirm e-mail</label>
+                <input type="email" name="email">
+            </p>
+            <p>
+                <label for="password">Your password</label>
+                <input type="password" name="password">
+            </p>
+        </form>
+    </div>
+</body>
+</html>
