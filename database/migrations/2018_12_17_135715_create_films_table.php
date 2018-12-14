96a9b2ea2c535e0e1856ed66c41817d3fd28e705
Roger Noel <noel.roger.pro@gmail.com>
2018-12-14 10:31:59 +0100

    création register, login et ajout des routes dans web.php

--- database/migrations/2018_12_17_135715_create_films_table.php
+++ database/migrations/2018_12_17_135715_create_films_table.php
@@ -1,5 +1,5 @@
 <?php
-// Made by Marco*********************************************
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -16,9 +16,9 @@ class CreateFilmsTable extends Migration
         Schema::create('films', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('link')->unique();
+            $table->string('link')->nullable();
             $table->string('poster')->nullable();
-            $table->integer('film-director')->unsigned();
+            $table->integer('film-director')->unsigned()->nullable();
             $table->foreign('film-director')->references('id')->on('film_directors')->onDelete('cascade');
             $table->date('release');
             $table->string('synopsis')->nullable();
@@ -36,4 +36,3 @@ class CreateFilmsTable extends Migration
         Schema::dropIfExists('films');
     }
 }
-// End made zone*********************************************
