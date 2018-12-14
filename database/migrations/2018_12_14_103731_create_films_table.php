3c6caa28842108fef4954fe22eab52348ad06321
Marco De Bona - AnarionBe <marcodb.debona@gmail.com>
2018-12-14 16:03:23 +0100

    ajout table films

---
+++ database/migrations/2018_12_14_103731_create_films_table.php
@@ -0,0 +1,37 @@
+<?php
+// Made by Marco*********************************************
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('link')->unique();
+            $table->string('poster');
+            $table->string('author');
+            $table->date('release');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
+// End made zone*********************************************
