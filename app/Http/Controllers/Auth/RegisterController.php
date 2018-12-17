fc88680121a8b6d57c5fc344cd3442c62cb2b67f
Marco De Bona - AnarionBe <marcodb.debona@gmail.com>
2018-12-17 16:43:57 +0100

    merge to commun

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
     {
         return Account::create([
             'email' => $data['email'],
-            'password' => bcrypt($data['password'], ['count' => 12]),
+            'password' => bcrypt($data['password']),
         ]);
     }
 }
