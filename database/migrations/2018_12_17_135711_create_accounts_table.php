9702b6a24799201c19259f402ae10f177d622b43
Marco De Bona - AnarionBe <marcodb.debona@gmail.com>
2018-12-17 15:57:41 +0100

    page register : retrait chanp name

--- database/migrations/2018_12_17_135711_create_accounts_table.php
+++ database/migrations/2018_12_17_135711_create_accounts_table.php
@@ -15,7 +15,6 @@ class CreateAccountsTable extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->increments('id');
-            //$table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
